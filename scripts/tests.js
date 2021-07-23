9f35c6b1da5344897e41c27edb4c92f65d3ef447
genesis-algorithms <genesisalgorithms@gmail.com>
2021-07-23 22:47:22 +0300

    Create test setup

---
+++ scripts/tests.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const homedir = require("os").homedir();
+const { execSync } = require("child_process");
+
+const { cmdsPath } = require("../bin/constants");
+
+console.log("backing up cmds.json");
+const cmdsBackup = `${homedir}/.cmds.backup.json`;
+fs.renameSync(cmdsPath, cmdsBackup);
+
+execSync("npm run jest", { stdio: "inherit" });
+
+console.log("\nrestoring cmds.json");
+fs.rmSync(cmdsPath);
+fs.renameSync(cmdsBackup, cmdsPath);
